a0639128381abfdb213620cf985d4690719f0318
Joab-Ran <joabraniel2006@gmail.com>
2024-11-15 17:45:08 -0300

    Add files via upload

---
+++ Prompt/js/index.js
@@ -0,0 +1,18 @@
+// Tecnicamente eu não precisaria fazer o IF já que como o fundo já é branco,
+// qualquer valor digitado no prompt que não seja entendido como um hexadecimal ou uma cor em inglês,
+// resultaria no fundo continuar branco. Eu testei.
+
+let corzinha = prompt("Digas patrão, qual a cor do fundo: Hexadecimal ou inglês")
+let body = document.querySelector(".body")
+
+function cor(color) {
+    if (color == "red" || color =="green" || color =="blue" || color == "#ff0000" || color == "#00ff00" || color == "#0000ff"){
+        body.style.backgroundColor = color;
+    } else {
+        alert("Cor inválida")
+        body.style.backgroundColor = white;
+    }
+    
+}
+
+cor(corzinha)
